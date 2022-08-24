20d0c09e3b57f5a93158dbff023cda5022f95a5a
Tordesillas <bubble83440@gmail.com>
2022-08-24 10:40:44 +0200

    Stars populate space

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import Svg, {Defs, RadialGradient, Rect, Stop} from 'react-native-svg';
+import Svg, {Circle, Defs, RadialGradient, Rect, Stop} from 'react-native-svg';
 
 export default class Background extends React.Component {
     shouldComponentUpdate(nextProps: any, nextState: any, nextContext: any): boolean {
@@ -19,6 +19,13 @@ export default class Background extends React.Component {
                             <Stop offset="90%" stopColor="#001718"/>
                             <Stop offset="100%" stopColor="#000808"/>
                         </RadialGradient>
+
+                        <RadialGradient id="star" cx={0.5} cy={0.5} r={0.5}>
+                            <Stop offset="0" stopColor="#FFFFFF" stopOpacity={0.9}/>
+                            <Stop offset="0.1" stopColor="#FFFFFF" stopOpacity={0.8}/>
+                            <Stop offset="0.4" stopColor="#FFFFFF" stopOpacity={0.3}/>
+                            <Stop offset="1" stopColor="#FFFFFF" stopOpacity={0}/>
+                        </RadialGradient>
                     </Defs>
                     <Rect
                         x="0"
@@ -27,6 +34,15 @@ export default class Background extends React.Component {
                         height='101%'
                         fill="url(#grad)"
                     />
+
+                    {[...Array(50)].map(() => (
+                        <Circle
+                            cx={Math.floor(Math.random() * 1000)}
+                            cy={Math.floor(Math.random() * 1000)}
+                            r={Math.floor(Math.random() * 10) + 1}
+                            fill="url(#star)"
+                        />
+                    ))}
                 </Svg>
             </View>
         );
